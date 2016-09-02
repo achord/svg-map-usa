7b44bbcb4ec92d7a4281124d6a77401cb2029eb5
Steven Achord <steven@trumpetgroup.com>
2016-09-02 10:15:14 -0500

    Working on map accuracy

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Plot multiple markers for the 48 contiguous U.S. states using latitude and longitude coordinates.  It utilizes vanilla javascript and SVG files instead of a map API.
 
 E.g.
-`<!-- New York City -->`
+`<!-- New York City -->` <br>
 `<i class="icon-marker" data-lat="40.712784" data-lng="-74.005941"></i>`
--- js/usMap.js
+++ js/usMap.js
@@ -4,7 +4,13 @@
 // t: 49.3931, r: -66.95, b: 24.545874, l: -124.75
 
 // Bounding Box must be exact in accordance with SVG in use. Also keep in mind the projection of the map. E.g. Mercator(Google maps)
-var USA = {t: 51.8, r: -66.95, b: 24.639528, l: -124.75};
+var usa = {
+  t: 49.3931,
+  r: -66.95,
+  b: 24.7,
+  l: -124.75
+};
+
 
 function setMarkers(){
   var marker = document.querySelectorAll('.icon-marker');
@@ -19,19 +25,27 @@ function setMarker(el, lat, long){
   var moduleWidth = document.getElementById('map-container').offsetWidth; // get width from css or from calculated
   var moduleHeight = document.getElementById('map-container').offsetHeight;
 
-  // Coordinate Bounding Box for LA
-  var top    =  USA.t;
-  var right  =  USA.r;
-  var bottom =  USA.b;
-  var left   =  USA.l;
+  // Coordinate Bounding Box for USA
+  var top    =  usa.t;
+  var right  =  usa.r;
+  var bottom =  usa.b;
+  var left   =  usa.l;
+
+  //Offset marker for centering
+  var markerHeight = el.offsetHeight;
+  var markerWidth = el.offsetWidth;
+
+  console.log(markerHeight, markerWidth);
 
   // Coordinate difference  
   var width  = (right - left);
   var height = (top - bottom) ;
   var latPx = (top - lat);
   var longPx = -(left - long);
-  var markerTop = Math.round ( (latPx / height) * moduleHeight ) -3 ; // offset to center marker
-  var markerLeft = Math.round( (longPx / width) * moduleWidth )  -3  ; // offset to center marker
+  //var markerTop = Math.round ( (latPx / height) * moduleHeight ) - ( markerHeight / 2); // offset to center marker
+  //var markerLeft = Math.round( (longPx / width) * moduleWidth )  - ( markerWidth / 2);  // offset to center marker
+  var markerTop = ( (latPx / height) * moduleHeight ) - ( markerHeight / 2); // offset to center marker
+  var markerLeft = ( (longPx / width) * moduleWidth )  - ( markerWidth / 2);  // offset to center marker
 
   //Set pos within div
   el.style.top  = markerTop + "px";
